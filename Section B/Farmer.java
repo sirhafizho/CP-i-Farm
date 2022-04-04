3d09fefd79ef4138bb96ca5e2055cf2b857085be
irfan-nasruddin <legendarychicken14@gmail.com>
2022-04-04 16:37:31 +0800

    Added farms variable, getter and setter for the variable and toString method for the class

--- Section B/Farmer.java
+++ Section B/Farmer.java
@@ -2,24 +2,26 @@ public class Farmer implements Runnable {
     private String _id;
     private String name;
     private String email;
-    // TODO: Determine what to do with the password variable
+    // TODO: Determine what to do with the password property
     private String password;
     private String phoneNumber;
+    private String[] farms;
 
-    Farmer(String id, String name, String email, String password, String phoneNumber) {
-        this._id = id;
+    Farmer(String _id, String name, String email, String password, String phoneNumber, String[] farms) {
+        this._id = _id;
         this.name = name;
         this.email = email;
         this.password= password;
         this.phoneNumber = phoneNumber;
+        this.farms = farms;
     }
 
     public String getId() {
         return _id;
     }
 
-    public void setId(String id) {
-        this._id = id;
+    public void setId(String _id) {
+        this._id = _id;
     }
 
     public String getName() {
@@ -46,6 +48,14 @@ public class Farmer implements Runnable {
         this.phoneNumber = phoneNumber;
     }
 
+    public String[] getFarms() {
+        return farms;
+    }
+
+    public void setFarms(String[] farms) {
+        this.farms = farms;
+    }
+
     @Override
     public void run() {
         // TODO: Implement activity class here
@@ -59,5 +69,16 @@ public class Farmer implements Runnable {
         for(int i=0; i< 1000; i++) {
             System.out.println(action + " " + type + " " + field + " " + row + " " + unit + " " + date);
         }
+    }
+
+    @Override
+    public String toString() {
+        String arrayString = "[";
+        for(int i = 0; i < farms.length - 1; i++) {
+            arrayString += farms[i] + ", ";
+        }
+        arrayString += farms[farms.length - 1] + "]";
+
+        return "[" + _id + ", " + name + ", " + email + ", " + password + ", " + phoneNumber + ", " + arrayString + "]";
     }
 }
