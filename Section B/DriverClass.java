a28a68a356aa4b8e9886d727ba88626e05af8758
irfan-nasruddin <legendarychicken14@gmail.com>
2022-04-04 16:36:18 +0800

    Updated the code in the DriverClass due to the change to the Farmer constructor

--- Section B/DriverClass.java
+++ Section B/DriverClass.java
@@ -1,6 +1,6 @@
 public class DriverClass {
     public static void main(String[] args) {
-        Runnable farmer = new Farmer("1", "ABCD", "abcd@example.com", "abcd12345", "012-3456789");
+        Runnable farmer = new Farmer("1", "ABCD", "abcd@example.com", "abcd12345", "012-3456789", new String[0]);
 
         Thread t1 = new Thread(farmer, "t1");
 
