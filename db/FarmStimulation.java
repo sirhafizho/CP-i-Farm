e86263d71f16942a1f4e72d7fd6e5e19746324a6
UbfamN <82451245+UbfamN@users.noreply.github.com>
2022-04-21 09:19:38 +0800

    Add files via upload
    Example of connection to database

---
+++ db/FarmStimulation.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package farmstimulation;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+
+
+public class FarmStimulation {
+
+    public static void main(String[] args) {
+        
+        
+        
+        try{
+            Connection con = null;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/i-farm","root","");
+            System.out.println("Connection is established successfully!");
+            Statement stmt = con.createStatement();
+//            stmt.executeUpdate("insert into fertilizers values ('3','NP 10 30 0','pack');");
+//            System.out.println("Record inserted successfully!");
+            ResultSet ferti = stmt.executeQuery("Select * from fertilizers;");
+            while(ferti.next()){
+                System.out.println(ferti.getInt("_id")+" | "+ferti.getString("name")+" | "+ferti.getString("unitType"));
+            }
+            stmt.close();
+            con.close();
+            
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+        
+        
+    }
+    
+}
