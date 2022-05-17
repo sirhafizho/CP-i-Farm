a548f7fd4250398301b4660f4e71658fc1bfa1d7
Fahad <ft.thakur1@gmail.com>
2022-05-17 23:34:00 +0800

    Created an activity class

---
+++ Section B/Activity.java
@@ -0,0 +1,29 @@
+public class Activity {
+    private String _id;
+    private String date;
+    private String action;
+    private String type;
+    private String unit;
+    private Integer quantity;
+    private Integer field;
+    private Integer row;
+    private Integer farmId;
+    private Integer userId;
+
+    
+    public Activity(String _id, String date, String action, String type, String unit, Integer quantity, Integer field,
+            Integer row, Integer farmId, Integer userId) {
+        this._id = _id;
+        this.date = date;
+        this.action = action;
+        this.type = type;
+        this.unit = unit;
+        this.quantity = quantity;
+        this.field = field;
+        this.row = row;
+        this.farmId = farmId;
+        this.userId = userId;
+    }
+
+
+}
