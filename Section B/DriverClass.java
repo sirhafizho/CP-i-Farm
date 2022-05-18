3179d8022c3e6611cc6817e2cfa4569a81901a9f
Kisyok <kisoindran@gmail.com>
2022-05-18 20:01:26 +0800

    random activity generation - seq approach

--- Section B/DriverClass.java
+++ Section B/DriverClass.java
@@ -1,35 +1,22 @@
+import java.time.Instant;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class DriverClass {
     public static void main(String[] args) {
-        // Runnable farmer = new Farmer("1", "ABCD", "abcd@example.com", "abcd12345", "012-3456789", new String[0]);
-
-        // Thread t1 = new Thread(farmer, "t1");
-
-        // t1.start();
-
-        // Establish the DB connection
-        // MysqlCon con = new MysqlCon();
-
-        // Farmer f = new Farmer(con.conn(), "1");
-        // Thread t = new Thread(f);
-        // t.start();
-
-        // // Close DB connection
-        // con.closeConn();
 
         FarmerSimulator simulator = new FarmerSimulator();
 
         Farmer[] farmers = simulator.generateFarmers(4);
 
-        // TODO: Sequential activity generation here
+        // // TODO: Sequential activity generation here
         for (Farmer farmer : farmers) {
             /*
                 Idea to use the farmer simulator class and add a sequential activity generation method
                 which accepts a Farmer as its parameter to generate the activities for the farmer
             */
             simulator.sequentialActivityGenerate(farmer);
-            
         }
-
 
     }
 }
--- Section B/FarmerSimulator.java
+++ Section B/FarmerSimulator.java
@@ -9,17 +9,25 @@ import java.util.Random;
 import java.util.Scanner;
 
 import java.sql.*;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class FarmerSimulator implements FarmerSimulatorInterface {
 
+    // to increment the activity id
     static int activityID = 1;
+
+     // Initailize a convenient Random object
+    Random random = new Random();
+
+    //initializing the mysql connection class
+    MysqlCon mysqlCon = new MysqlCon();
+    Statement stmt = mysqlCon.conn();
 
     // This method generates a number of farmers
     @Override
     public Farmer[] generateFarmers(int numberOfFarmers)
     {   
-        // Initailize a convenient Random object
-        Random random = new Random();
 
         // Prepare variables required for the generation of the details of the farmers
         Farmer[] farmers = new Farmer[numberOfFarmers];
@@ -28,9 +36,6 @@ public class FarmerSimulator implements FarmerSimulatorInterface {
         final int MAX_FARM_NUMBER = 5;
         BinaryTree binaryTree = null;
 
-        //initializing the mysql connection class
-        MysqlCon mysqlCon = new MysqlCon();
-        Statement stmt = mysqlCon.conn();
         String preparedSQL = "INSERT INTO Users(_id, name, email, password, phoneNumber, farms) "+ "VALUES(?,?,?,?,?,?)";
 
         try {
@@ -96,8 +101,7 @@ public class FarmerSimulator implements FarmerSimulatorInterface {
                         // Get the name from the JSON response
                         name = sb.substring(9, sb.indexOf("\"", 9));
                     }
-                } 
-                catch (Exception e) {
+                } catch (Exception e) {
                     // TODO: Do something with the error
                     System.out.println("Name API didn't work");
                     System.exit(1);
@@ -127,25 +131,146 @@ public class FarmerSimulator implements FarmerSimulatorInterface {
 
         } catch(SQLException e){
             System.out.println(e.getMessage());
-        } finally{
-            System.out.println("Closing database connection");
-            mysqlCon.closeConn();
-        }
+        } 
+        // finally{
+        //     System.out.println("Closing database connection");
+        //     mysqlCon.closeConn();
+        // }
 
         return farmers;
     }
 
+    // This method generates random date
+    public String generateDate() {
+        NumberFormat formatter = new DecimalFormat("00");
+        String year = formatter.format(random.nextInt(22));
+        String month = formatter.format(1 + random.nextInt(12));
+        String day = formatter.format(1 + random.nextInt(28));
+        String date = "20" + year + "-" + month + "-" + day;
+        return date;
+    }
+
     //This method simulates the sequential activity generation
     public void sequentialActivityGenerate(Farmer farmer){
-        // TODO: Sequential activity generation
 
-        System.out.println("Farmer " + farmer.getId() + "; Farms " + Arrays.toString(farmer.getFarms()));
+        System.out.println("Farmer " + farmer.getId() + ": Farms " + Arrays.toString(farmer.getFarms()));
         
-        for (String farms : farmer.getFarms()) {
-            // generate activities for each farms
-            for(int i=0; i<10; i++){
-                System.out.println(activityID);
-                activityID++;
+        for(String farm : farmer.getFarms()){
+
+            
+            String[] plants = new String[0];
+            String[] fertilizers = new String[0];
+            String[] pesticides = new String[0];
+
+            // get the array of plants, pesticides and fertilizers of the farm
+            try {  
+                ResultSet rs = stmt.executeQuery("select * from farms where _id =" + farm);  
+                while(rs.next()) {
+                    plants = rs.getString("plants").split(",");
+                    fertilizers = rs.getString("fertilizers").split(",");
+                    pesticides = rs.getString("pesticides").split(",");
+                }
+            } catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
+
+            // to get random number of activities
+            int numOfActivity = 1 + random.nextInt(12);
+            System.out.println("Farmer " + farmer.getId() + " generates " + numOfActivity + " activities of random types for Farm " + farm);
+
+            // to generate random activities
+            for(int i=0; i<numOfActivity; i++){
+                String date = generateDate();
+
+                int act = 1 + random.nextInt(5);
+                String action = "";
+                String type = "";
+                String unit = "kg";
+                int quantity = 1 + random.nextInt(10);
+                int field = 1 + random.nextInt(10);
+                int row = 1 + random.nextInt(10);
+
+                if(act == 1) {
+                    action = "sowing";
+      	            String plantId = plants[random.nextInt(plants.length)];
+                    try {  
+                        ResultSet rs = stmt.executeQuery("select * from plants where _id =" + plantId);  
+                        while(rs.next()) {
+                            type = rs.getString("name");
+                        }
+                    } catch(SQLException e){
+                        System.out.println(e.getMessage());
+                    }
+                } else if(act == 2) {
+                    action = "harvest";
+                    String plantId = plants[random.nextInt(plants.length)];
+                    try {  
+                        ResultSet rs = stmt.executeQuery("select * from plants where _id =" + plantId);  
+                        while(rs.next()) {
+                            type = rs.getString("name");
+                        }
+                    } catch(SQLException e){
+                        System.out.println(e.getMessage());
+                    }
+                } else if(act == 3) {
+                    action = "pesticide";
+                    String pesticideId = pesticides[random.nextInt(pesticides.length)];
+                    try {  
+                        ResultSet rs = stmt.executeQuery("select * from pesticides where _id =" + pesticideId);  
+                        while(rs.next()) {
+                            type = rs.getString("name");
+                        }
+                    } catch(SQLException e){
+                        System.out.println(e.getMessage());
+                    }
+                } else if(act == 4) {
+                    action = "fertilizer";
+                    String fertilizerId = fertilizers[random.nextInt(fertilizers.length)];
+                    try {  
+                        ResultSet rs = stmt.executeQuery("select * from fertilizers where _id =" + fertilizerId);  
+                        while(rs.next()) {
+                            type = rs.getString("name");
+                        }
+                    } catch(SQLException e){
+                        System.out.println(e.getMessage());
+                    }
+                } else {
+                    action = "sales";
+                    String plantId = plants[random.nextInt(plants.length)];
+                    try {  
+                        ResultSet rs = stmt.executeQuery("select * from plants where _id =" + plantId);  
+                        while(rs.next()) {
+                            type = rs.getString("name");
+                        }
+                    } catch(SQLException e){
+                        System.out.println(e.getMessage());
+                    }
+                } 
+
+                try{
+
+                    // preparing the insert query to insert activity into database
+                    String preparedSQL = "INSERT INTO activities(_id, date, action, type, unit, quantity, field, row, farmId, userId) "+ "VALUES(?,?,?,?,?,?,?,?,?,?)";
+                    PreparedStatement pstmt = mysqlCon.getCon().prepareStatement(preparedSQL);   
+
+                    pstmt.setString(1, Integer.toString(activityID));
+                    pstmt.setString(2, date);
+                    pstmt.setString(3, action);
+                    pstmt.setString(4, type);
+                    pstmt.setString(5, unit);
+                    pstmt.setInt(6, quantity);
+                    pstmt.setInt(7, field);
+                    pstmt.setInt(8, row);
+                    pstmt.setString(9, farm);
+                    pstmt.setString(10, farmer.getId());
+
+                    pstmt.executeUpdate();
+
+                    activityID++;
+                    
+                }catch(SQLException e){
+                    System.out.println(e.getMessage());
+                }
             }
         }
     }
